c112a86808e37462faae267a2130d884cc6c27be
juanchovanegas <jsvanegas@airsvanegas.local>
2017-02-02 12:29:55 -0500

    Se agregan ejemplos básicos de filltable y apptable.

--- Symfony/src/FrontendBundle/Controller/DefaultController.php
+++ Symfony/src/FrontendBundle/Controller/DefaultController.php
@@ -10,4 +10,17 @@ class DefaultController extends Controller
     {
         return $this->render('FrontendBundle:Default:index.html.twig');
     }
+
+    public function ejemploBlancoAction()
+    {
+        return $this->render('FrontendBundle:Default:pagina_ejemplo.html.twig');
+    }
+
+    public function filltableAction(){
+    	return $this->render('FrontendBundle:Default:filltable.html.twig');	
+    }
+
+    public function apptableAction(){
+    	return $this->render('FrontendBundle:Default:apptable.html.twig');	
+    }
 }
